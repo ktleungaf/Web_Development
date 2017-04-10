ee7c92be117a59322da5a50b63a0ca808628200e
ktleungaf <ktleungaf@connect.ust.hk>
2017-04-10 23:49:51 +0800

    added C# management system web application

---
+++ SinExWebApp20273938 (Activity 1)/SinExWebApp20273938/Controllers/PersonalShippingAccountsController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SinExWebApp20273938.Models;
+
+namespace SinExWebApp20273938.Controllers
+{
+    public class PersonalShippingAccountsController : Controller
+    {
+        private SinExDatabaseContext db = new SinExDatabaseContext();
+
+        // GET: PersonalShippingAccounts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonalShippingAccounts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ShippingAccountId,Building,Street,City,ProvinceCode,PostalCode,Type,CardNumber,SecurityNumber,CardholderName,ExpiryMonth,ExpiryYear,PhoneNumber,EmailAddress,AccountNumber,FirstName,LastName")] PersonalShippingAccount personalShippingAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ShippingAccounts.Add(personalShippingAccount);
+                db.SaveChanges();
+                return RedirectToAction("Home");
+            }
+
+            return View(personalShippingAccount);
+        }
+
+        // GET: PersonalShippingAccounts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonalShippingAccount personalShippingAccount = (PersonalShippingAccount) db.ShippingAccounts.Find(id);
+            if (personalShippingAccount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personalShippingAccount);
+        }
+
+        // POST: PersonalShippingAccounts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ShippingAccountId,Building,Street,City,ProvinceCode,PostalCode,Type,CardNumber,SecurityNumber,CardholderName,ExpiryMonth,ExpiryYear,PhoneNumber,EmailAddress,AccountNumber,FirstName,LastName")] PersonalShippingAccount personalShippingAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(personalShippingAccount).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(personalShippingAccount);
+        }
+
+        // POST: PersonalShippingAccounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PersonalShippingAccount personalShippingAccount = (PersonalShippingAccount)db.ShippingAccounts.Find(id);
+            db.ShippingAccounts.Remove(personalShippingAccount);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SinExWebApp20273938 (Activity 1)/SinExWebApp20273938/Models/Shipment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SinExWebApp20273938.Models
+{
+    [Table("Shipment")]
+    public class Shipment
+    {   
+        // 11 attributes
+        [Key]
+        public virtual int WaybillId { get; set; }
+        public virtual string ReferenceNumber { get; set; }     //this attribute is not displayed in the webpage but needed for data retrieval.
+        public virtual string ServiceType { get; set; }
+        public virtual DateTime ShippedDate { get; set; }
+        public virtual DateTime DeliveredDate { get; set; }
+        public virtual string RecipientName { get; set; }
+        public virtual int NumberOfPackages { get; set; }
+        public virtual string Origin { get; set; }
+        public virtual string Destination { get; set; }
+        public virtual string Status { get; set; }           //this attribute is not displayed in the webpage but needed for data retrieval.
+
+        // public virtual int ShippingAccountId { get; set; }
+
+            //lab06 part 1
+        //Navigation property to Shipment. 1 on the ShippingAccount side and N on the Shipment side
+        // Foreign key references ShippingAccount.
+        //[ForeignKey("ShippingAccount")]
+        //[DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public virtual int ShippingAccountId { get; set; }
+        //Navigation property to ShippingAccount. 1 on the ShippingAccount side and N on the Shipment side
+        public virtual ShippingAccount ShippingAccount { get; set; }
+    }
+}
